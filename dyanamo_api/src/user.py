383f5fa02d03c305911e2c5728bf53a4cffd6356
EC2 Default User <ec2-user@ip-172-31-3-109.ec2.internal>
2021-06-15 03:46:49 +0000

    Finished CloudFormation dynamo api

--- dyanamo_api/src/user.py
+++ dyanamo_api/src/user.py
@@ -2,37 +2,54 @@ import json
 import boto3
 import os
 
-users_table = os.environ('USERS_TABLE')
+users_table = os.environ['USERS_TABLE']
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(users_table)
+
 def getUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    user_id = array_path[-1]
+    
     response = table.get_item(
         Key={
-            'pk': 'user_',
+            'pk': user_id,
             'sk': 'age'
         }
     )
-    item = response('Item')
+    item = response['Item']
     print(item)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(item)
     }
 
 def putUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
-    response = table.put_item(
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    user_id = array_path[-1]
+    
+    body = event["body"]
+    body_object = json.loads(body)
+    
+    table.put_item(
         Item={
-            'pk': 'user_',
-            'sk': 'age'
+            'pk': user_id,
+            'sk': 'age',
+            'name': body_object['name'],
+            'last_name': body_object["last_name"],
+            'age': body_object['age']
         }
     )
-    print(item)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('User saved')
     }
